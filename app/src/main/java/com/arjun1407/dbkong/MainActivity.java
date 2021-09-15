57fa860345c64320ab93bd575a0694ce8352b144
Arjun <arjun140702@gmail.com>
2021-09-15 14:28:17 +0530

    Configure CMakeLists.txt, Load libraries into MainActivity

--- app/src/main/java/com/arjun1407/dbkong/MainActivity.java
+++ app/src/main/java/com/arjun1407/dbkong/MainActivity.java
@@ -12,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'dbkong' library on application startup.
     static {
         System.loadLibrary("dbkong");
+        System.loadLibrary("native-lib");
+        System.loadLibrary("node");
     }
 
     private ActivityMainBinding binding;
+    public static boolean _startedNodeAlready=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +27,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        //TextView tv = binding.sampleText;
+        //tv.setText(stringFromJNI());
+
+        if( !_startedNodeAlready ) {
+            _startedNodeAlready=true;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    startNodeWithArguments(new String[]{});
+                }
+            }).start();
+        }
     }
 
-    /**
+    /*
      * A native method that is implemented by the 'dbkong' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
+    public native Integer startNodeWithArguments(String[] arguments);
 }
