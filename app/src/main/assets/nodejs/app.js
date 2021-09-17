ee4d3e24d79ecfc4995bdbe5f31e2b850924e465
Arjun <arjun140702@gmail.com>
2021-09-17 17:15:52 +0530

    Add helper functions

--- app/src/main/assets/nodejs/app.js
+++ app/src/main/assets/nodejs/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello there");
+})
+
 app.listen(3000, () => {
     console.log("Server started at port 3000");
 })
