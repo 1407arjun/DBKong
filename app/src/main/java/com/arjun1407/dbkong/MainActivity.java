ea3cc8bedb201b0c691f2856d86dd75a003ff518
Arjun <arjun140702@gmail.com>
2021-09-18 19:30:18 +0530

    Code formatting

--- app/src/main/java/com/arjun1407/dbkong/MainActivity.java
+++ app/src/main/java/com/arjun1407/dbkong/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.arjun1407.dbkong.databinding.ActivityMainBinding;
+import com.arjun1407.dbkong.utility.HelperClass;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        HelperClass helper = new HelperClass(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -56,16 +58,16 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     //The path where we expect the node project to be at runtime.
                     String nodeDir=getApplicationContext().getFilesDir().getAbsolutePath()+"/nodejs";
-                    if (wasAPKUpdated()) {
+                    if (helper.wasAPKUpdated()) {
                         //Recursively delete any existing nodejs-project.
                         File nodeDirReference=new File(nodeDir);
                         if (nodeDirReference.exists()) {
-                            deleteFolderRecursively(new File(nodeDir));
+                            helper.deleteFolderRecursively(new File(nodeDir));
                         }
                         //Copy the node project from assets into the application's data path.
-                        copyAssetFolder(getApplicationContext().getAssets(), "nodejs", nodeDir);
+                        helper.copyAssetFolder(getApplicationContext().getAssets(), "nodejs", nodeDir);
 
-                        saveLastUpdateTime();
+                        helper.saveLastUpdateTime();
                     }
                     startNodeWithArguments(new String[]{"node",
                             nodeDir+"/app.js"
@@ -74,38 +76,7 @@ public class MainActivity extends AppCompatActivity {
             }).start();
         }
 
-        new CountDownTimer(10000, 1) {
-            @Override
-            public void onTick(long l) {
 
-            }
-
-            @Override
-            public void onFinish() {
-                new AsyncTask<Void,Void,String>() {
-                    @Override
-                    protected String doInBackground(Void... params) {
-                        String nodeResponse="";
-                        try {
-                            URL localNodeServer = new URL("http://localhost:3000/");
-                            BufferedReader in = new BufferedReader(
-                                    new InputStreamReader(localNodeServer.openStream()));
-                            String inputLine;
-                            while ((inputLine = in.readLine()) != null)
-                                nodeResponse=nodeResponse+inputLine;
-                            in.close();
-                        } catch (Exception ex) {
-                            nodeResponse=ex.toString();
-                        }
-                        return nodeResponse;
-                    }
-                    @Override
-                    protected void onPostExecute(String result) {
-                        textView.setText(result);
-                    }
-                }.execute();
-            }
-        }.start();
     }
 
     /*
@@ -115,98 +86,5 @@ public class MainActivity extends AppCompatActivity {
     //public native String stringFromJNI();
     public native int startNodeWithArguments(String[] arguments);
 
-    private static boolean deleteFolderRecursively(File file) {
-        try {
-            boolean res=true;
-            for (File childFile : file.listFiles()) {
-                if (childFile.isDirectory()) {
-                    res &= deleteFolderRecursively(childFile);
-                } else {
-                    res &= childFile.delete();
-                }
-            }
-            res &= file.delete();
-            return res;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 
-    private static boolean copyAssetFolder(AssetManager assetManager, String fromAssetPath, String toPath) {
-        try {
-            String[] files = assetManager.list(fromAssetPath);
-            boolean res = true;
-
-            if (files.length==0) {
-                //If it's a file, it won't have any assets "inside" it.
-                res &= copyAsset(assetManager,
-                        fromAssetPath,
-                        toPath);
-            } else {
-                new File(toPath).mkdirs();
-                for (String file : files)
-                    res &= copyAssetFolder(assetManager,
-                            fromAssetPath + "/" + file,
-                            toPath + "/" + file);
-            }
-            return res;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    private static boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = assetManager.open(fromAssetPath);
-            new File(toPath).createNewFile();
-            out = new FileOutputStream(toPath);
-            copyFile(in, out);
-            in.close();
-            in = null;
-            out.flush();
-            out.close();
-            out = null;
-            return true;
-        } catch(Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    private static void copyFile(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int read;
-        while ((read = in.read(buffer)) != -1) {
-            out.write(buffer, 0, read);
-        }
-    }
-
-    private boolean wasAPKUpdated() {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
-        long previousLastUpdateTime = prefs.getLong("NODEJS_MOBILE_APK_LastUpdateTime", 0);
-        long lastUpdateTime = 1;
-        try {
-            PackageInfo packageInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
-            lastUpdateTime = packageInfo.lastUpdateTime;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return (lastUpdateTime != previousLastUpdateTime);
-    }
-
-    private void saveLastUpdateTime() {
-        long lastUpdateTime = 1;
-        try {
-            PackageInfo packageInfo = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0);
-            lastUpdateTime = packageInfo.lastUpdateTime;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
-        prefs.edit().putLong("NODEJS_MOBILE_APK_LastUpdateTime", lastUpdateTime).apply();
-    }
 }
---
+++ app/src/main/java/com/arjun1407/dbkong/utility/HelperClass.java
@@ -0,0 +1,118 @@
+package com.arjun1407.dbkong.utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class HelperClass {
+    
+    private final Context context;
+
+    public HelperClass(Context context) {
+        this.context = context;
+    }
+
+    public boolean deleteFolderRecursively(File file) {
+        try {
+            boolean res=true;
+            for (File childFile : file.listFiles()) {
+                if (childFile.isDirectory()) {
+                    res &= deleteFolderRecursively(childFile);
+                } else {
+                    res &= childFile.delete();
+                }
+            }
+            res &= file.delete();
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean copyAssetFolder(AssetManager assetManager, String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            boolean res = true;
+
+            if (files.length==0) {
+                //If it's a file, it won't have any assets "inside" it.
+                res &= copyAsset(assetManager,
+                        fromAssetPath,
+                        toPath);
+            } else {
+                new File(toPath).mkdirs();
+                for (String file : files)
+                    res &= copyAssetFolder(assetManager,
+                            fromAssetPath + "/" + file,
+                            toPath + "/" + file);
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean copyAsset(AssetManager assetManager, String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(fromAssetPath);
+            new File(toPath).createNewFile();
+            out = new FileOutputStream(toPath);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    public boolean wasAPKUpdated() {
+        SharedPreferences prefs = context.getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
+        long previousLastUpdateTime = prefs.getLong("NODEJS_MOBILE_APK_LastUpdateTime", 0);
+        long lastUpdateTime = 1;
+        try {
+            PackageInfo packageInfo = context.getApplicationContext().getPackageManager().getPackageInfo(context.getApplicationContext().getPackageName(), 0);
+            lastUpdateTime = packageInfo.lastUpdateTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return (lastUpdateTime != previousLastUpdateTime);
+    }
+
+    public void saveLastUpdateTime() {
+        long lastUpdateTime = 1;
+        try {
+            PackageInfo packageInfo = context.getApplicationContext().getPackageManager().getPackageInfo(context.getApplicationContext().getPackageName(), 0);
+            lastUpdateTime = packageInfo.lastUpdateTime;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        SharedPreferences prefs = context.getApplicationContext().getSharedPreferences("NODEJS_MOBILE_PREFS", Context.MODE_PRIVATE);
+        prefs.edit().putLong("NODEJS_MOBILE_APK_LastUpdateTime", lastUpdateTime).apply();
+    }
+    
+}
