511e8a476b551aa365b66a99f348a1eb71190ed2
Tanmay Vyas <70322519+Tanmay000009@users.noreply.github.com>
2021-09-21 17:05:20 +0530

    Updated this to super

--- app/src/main/java/com/arjun1407/dbkong/database/mongodb/Collection.java
+++ app/src/main/java/com/arjun1407/dbkong/database/mongodb/Collection.java
@@ -3,14 +3,14 @@ package com.arjun1407.dbkong.database.mongodb;
 public class Collection extends MongoDBDao {
 
     public Collection(String collection) {
-        this.collection = collection;
+        super.collection = collection;
     }
 
     protected Collection() {
     }
 
     public Collection collection(String collection) {
-        this.collection = collection;
+        super.collection = collection;
         return new Collection(collection);
     }
 }
